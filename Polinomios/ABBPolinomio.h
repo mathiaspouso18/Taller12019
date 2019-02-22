49090c63f7b45a8e97387869eb5da9479f11fbeb
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2019-02-22 16:28:51 -0300

    Update de funciones y cabezales

--- Polinomios/ABBPolinomio.h
+++ Polinomios/ABBPolinomio.h
@@ -10,9 +10,13 @@ typedef struct nodoN { Polinomio info;
 typedef nodo *Arbol;
 
 void InicializarArbol(Arbol &ABB);
+Boolean EsVacio(Arbol ABB);
+Polinomio DarRaiz(Arbol ABB);
+Arbol HijoIzquierdo(Arbol ABB);
+Arbol HijoDerecho(Arbol ABB);
 void MostrarABBPoli(Arbol ABBPoli);
 void InsPoliABBPoli(Arbol &ABBPoli, Polinomio p);
 TipoMensaje ExistePolinomio(Arbol &ABBPoli, String nombre_poli);
-void DarPolinomio (Arbol &ABBPoli, String nombre_poli);
+Polinomio DarPolinomio (Arbol ABBPoli, String nombre_poli);
 
 #endif // ABBPOLINOMIO_H_INCLUDED
