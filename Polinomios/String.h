e666d6697d0e85212bf477a4f78181e9489dd239
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2019-02-26 17:00:30 -0300

    Funcion LowerCase

--- Polinomios/String.h
+++ Polinomios/String.h
@@ -46,6 +46,8 @@ Boolean ValidarNombreArchivo (String s);
 
 Boolean EsMayor(String s1, String s2);
 
+void LowerCase(String &s);
+
 void Guardar_String (String s, FILE * f) ;
 
 void Levantar_String (String &s, FILE * f);
