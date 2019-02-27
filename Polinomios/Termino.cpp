ca20e56d3493ced3a905640e2ab32934fa6c8fc6
Reaperwest <45336070+Reaperwest@users.noreply.github.com>
2019-02-27 16:56:49 -0300

    Avance de bajada a archivos

--- Polinomios/Termino.cpp
+++ Polinomios/Termino.cpp
@@ -18,3 +18,32 @@ void CrearTermino(Termino &t, int base, int exp, char signo)
     t.base = base;
     t.exponente = exp;
 }
+
+void ConvertirTerminoAString(Termino term, String &s)
+{
+	char signo = DarSigno(term);
+	int base = DarBase(term), i = 0;
+	String sbase;
+
+	if(s[0] = '\0')
+	{
+		s[0] = signo;
+		s[1] = '\0';
+		IntToString(sbase,base);
+		StrCon(s,sbase);
+	}
+	//En proceso
+	while(s != '\0')
+	{
+		
+	}
+	
+}
+
+Termino ConvertirStringATermino (String s)
+{
+	//En proceso
+	Termino term;
+
+	return term;
+}
